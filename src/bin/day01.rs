8cd51aa70bca97646ab030250a76d3d34b90b2c5
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2021-12-02 20:33:34 +0100

    Add tests to day 1 solution

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -28,3 +28,43 @@ fn main() -> Result<(), std::io::Error> {
     println!("{}", increments);
     Ok(())
 }
+
+#[cfg(test)]
+mod tests {
+    use super::*;
+
+    const TEST_INPUT: &'static str = "199\n\
+        200\n\
+        208\n\
+        210\n\
+        200\n\
+        207\n\
+        240\n\
+        269\n\
+        260\n\
+        263";
+
+    #[test]
+    fn input_parses_correctly() {
+        println!("{}", TEST_INPUT);
+        let numbers = parse_input(TEST_INPUT.into());
+        assert_eq!(
+            numbers,
+            vec![199, 200, 208, 210, 200, 207, 240, 269, 260, 263]
+        );
+    }
+
+    #[test]
+    fn counts_increments() {
+        let numbers = parse_input(TEST_INPUT.into());
+        let increments = count_increments(&numbers);
+        assert_eq!(increments, 7);
+    }
+
+    #[test]
+    fn counts_increments_in_triplets() {
+        let numbers = parse_input(TEST_INPUT.into());
+        let increments = count_increments_triplets(&numbers);
+        assert_eq!(increments, 5);
+    }
+}
