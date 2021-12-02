f19dabb3c6da14b59c41ce76a72205e1647f3d8d
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2021-12-02 18:56:28 +0100

    Add solution for day 1

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -1,6 +1,18 @@
 use aoc::input::read_input;
 
-fn main() {
-    let input = read_input();
-    println!("{:?}", input);
+fn parse_input(input: String) -> Vec<u32> {
+    input.lines().map(|l| l.parse().unwrap()).collect()
+}
+
+fn main() -> Result<(), std::io::Error> {
+    let input = read_input()?;
+    let numbers = parse_input(input);
+
+    // part 2
+    let numbers: Vec<u32> = numbers.windows(3).map(|w| w.iter().sum()).collect();
+
+    let increments = numbers.windows(2).filter(|pair| pair[1] > pair[0]).count();
+
+    println!("{}", increments);
+    Ok(())
 }
