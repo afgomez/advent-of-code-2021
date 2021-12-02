c997b9865a6b5d363d3b45f914a937eddd9cdd95
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2021-12-02 20:33:18 +0100

    Refactor day 1 solution

--- src/bin/day01.rs
+++ src/bin/day01.rs
@@ -4,14 +4,26 @@ fn parse_input(input: String) -> Vec<u32> {
     input.lines().map(|l| l.parse().unwrap()).collect()
 }
 
+// Part 1
+fn count_increments(numbers: &Vec<u32>) -> usize {
+    numbers.windows(2).filter(|pair| pair[1] > pair[0]).count()
+}
+
+// Part 2
+fn count_increments_triplets(numbers: &Vec<u32>) -> usize {
+    let triplets: Vec<u32> = numbers.windows(3).map(|w| w.iter().sum()).collect();
+    count_increments(&triplets)
+}
+
 fn main() -> Result<(), std::io::Error> {
     let input = read_input()?;
     let numbers = parse_input(input);
 
-    // part 2
-    let numbers: Vec<u32> = numbers.windows(3).map(|w| w.iter().sum()).collect();
+    // Part 1
+    // let increments = count_increments(&numbers);
 
-    let increments = numbers.windows(2).filter(|pair| pair[1] > pair[0]).count();
+    // Part 2
+    let increments = count_increments_triplets(&numbers);
 
     println!("{}", increments);
     Ok(())
