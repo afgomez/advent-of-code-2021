e6c07631e58bdc1633abe235fe870bc6e51106bf
Alejandro Fernández Gómez <alejandro.fernandez@elastic.co>
2021-12-03 22:08:26 +0100

    Move functions to `Telemetry` methods

--- src/bin/day03.rs
+++ src/bin/day03.rs
@@ -1,102 +1,114 @@
 use aoc::input::read_input;
 
-fn parse_input<T: AsRef<str>>(input: T) -> (Vec<String>, usize) {
-    let entries: Vec<String> = input.as_ref().lines().map(str::to_owned).collect();
-    let bit_count = if entries.len() == 0 {
-        0
-    } else {
-        entries.get(0).unwrap().len()
-    };
-
-    (entries, bit_count)
+struct Telemetry {
+    entries: Vec<String>,
+    entry_len: usize,
 }
 
-fn calculate_consumption<T: AsRef<[String]>>(data: T, bit_count: usize) -> u32 {
-    let data = data.as_ref();
-    let len: u32 = data.len().try_into().unwrap();
+impl Telemetry {
+    fn new() -> Self {
+        Telemetry {
+            entries: vec![],
+            entry_len: 0,
+        }
+    }
 
-    // Count the amount of '1' per bit
-    let mut bit_counter: Vec<u32> = vec![0; bit_count];
-    for entry in data.iter() {
-        for (i, bit) in entry.chars().enumerate() {
-            if bit == '1' {
-                *(bit_counter.get_mut(i).unwrap()) += 1;
+    fn from<T: AsRef<str>>(input: T) -> Self {
+        let mut telemetry = Telemetry::new();
+        telemetry.parse(input);
+        telemetry
+    }
+
+    fn parse<T: AsRef<str>>(&mut self, input: T) {
+        let entries: Vec<String> = input.as_ref().lines().map(str::to_owned).collect();
+        let entry_len = if entries.len() == 0 {
+            0
+        } else {
+            entries.get(0).unwrap().len()
+        };
+
+        self.entries = entries;
+        self.entry_len = entry_len;
+    }
+
+    fn consumption(&self) -> usize {
+        let len = self.entries.len();
+
+        let mut ones_per_position: Vec<usize> = vec![0; self.entry_len];
+        for entry in &self.entries {
+            for (pos, bit) in entry.chars().enumerate() {
+                if bit == '1' {
+                    *(ones_per_position.get_mut(pos).unwrap()) += 1;
+                }
             }
         }
+
+        let most_common_bit_per_position: String = ones_per_position
+            .into_iter()
+            .map(|no_ones| if no_ones > len / 2 { '1' } else { '0' })
+            .collect();
+
+        // `gamma` is the most common bit per position
+        let gamma_rate = usize::from_str_radix(&most_common_bit_per_position, 2).unwrap();
+
+        // `epsilon` is the least common bit per position, that is, the bit inverse of gamma
+        let mask = 2_usize.pow(self.entry_len.try_into().unwrap()) - 1; // 0b1000 - 1 = 0b111, for a `entry_len` of 3
+        let epsilon_rate = gamma_rate ^ mask;
+
+        gamma_rate * epsilon_rate
     }
 
-    let gamma_rate_bits: Vec<u32> = bit_counter
-        .into_iter()
-        .map(|count| if count > len / 2 { 1 } else { 0 })
-        .collect();
+    fn life_support(&self) -> usize {
+        let mut entries = self.entries.to_vec();
+        for pos in 0..self.entry_len {
+            let (entries_with_zero, entries_with_one) = partition_by_bit_at_pos(&entries, pos);
 
-    // This can be collapsed with the previous iterator
-    // Left in two for legibility
-    let gamma_rate: u32 = gamma_rate_bits
-        .iter()
-        .rev()
-        .enumerate()
-        .fold(0, |rate, (pos, bit)| rate + (bit << pos));
+            entries = if entries_with_one.len() >= entries_with_zero.len() {
+                entries_with_one
+            } else {
+                entries_with_zero
+            };
 
-    let epsilon_rate = gamma_rate ^ 2_u32.pow(bit_count as u32) - 1;
-    gamma_rate * epsilon_rate
+            if entries.len() == 1 {
+                break;
+            }
+        }
+        let o2 = usize::from_str_radix(&entries[0], 2).unwrap();
+
+        let mut entries = self.entries.to_vec();
+        for pos in 0..self.entry_len {
+            let (entries_with_zero, entries_with_one) = partition_by_bit_at_pos(&entries, pos);
+
+            entries = if entries_with_one.len() < entries_with_zero.len() {
+                entries_with_one
+            } else {
+                entries_with_zero
+            };
+
+            if entries.len() == 1 {
+                break;
+            }
+        }
+
+        let co2 = usize::from_str_radix(&entries[0], 2).unwrap();
+
+        o2 * co2
+    }
 }
 
-fn calculate_life_support<T: AsRef<[String]>>(data: T, bit_count: usize) -> u32 {
-    let mut filtered: Vec<String> = data.as_ref().to_vec();
-
-    // Calculate oxigen
-    for i in 0..bit_count {
-        let (leading_ones, leading_zeroes): (Vec<_>, Vec<_>) =
-            filtered.into_iter().partition(|entry| {
-                let bit = entry.chars().nth(i).unwrap();
-                bit == '1'
-            });
-
-        if leading_ones.len() >= leading_zeroes.len() {
-            filtered = leading_ones;
-        } else {
-            filtered = leading_zeroes;
-        }
-
-        if filtered.len() == 1 {
-            break;
-        }
-    }
-
-    let oxigen = u32::from_str_radix(filtered.get(0).unwrap(), 2).unwrap();
-
-    // Calculate co2
-    let mut filtered = data.as_ref().to_vec();
-    for i in 0..bit_count {
-        let (leading_ones, leading_zeroes): (Vec<_>, Vec<_>) =
-            filtered.into_iter().partition(|entry| {
-                let bit = entry.chars().nth(i).unwrap();
-                bit == '1'
-            });
-
-        if leading_ones.len() < leading_zeroes.len() {
-            filtered = leading_ones;
-        } else {
-            filtered = leading_zeroes;
-        }
-
-        if filtered.len() == 1 {
-            break;
-        }
-    }
-
-    let co2 = u32::from_str_radix(filtered.get(0).unwrap(), 2).unwrap();
-
-    oxigen * co2
+fn partition_by_bit_at_pos(entries: &Vec<String>, pos: usize) -> (Vec<String>, Vec<String>) {
+    entries
+        .to_vec()
+        .into_iter()
+        .partition(|entry| entry.chars().nth(pos).unwrap() == '0')
 }
 
 fn main() -> Result<(), std::io::Error> {
     let input = read_input()?;
-    let (entries, bit_count) = parse_input(input);
+    let telemetry = Telemetry::from(&input);
 
-    let consumption = calculate_consumption(&entries, bit_count);
-    let life_support = calculate_life_support(&entries, bit_count);
+    let consumption = telemetry.consumption();
+    let life_support = telemetry.life_support();
 
     println!("{}", consumption);
     println!("{}", life_support);
@@ -121,10 +133,11 @@ mod tests {
         01010";
     #[test]
     fn it_parses_the_input() {
-        let (entries, bit_count) = parse_input(TEST_INPUT);
-        assert_eq!(bit_count, 5);
+        let telemetry = Telemetry::from(TEST_INPUT);
+
+        assert_eq!(telemetry.entry_len, 5);
         assert_eq!(
-            entries,
+            telemetry.entries,
             vec![
                 "00100", "11110", "10110", "10111", "10101", "01111", "00111", "11100", "10000",
                 "11001", "00010", "01010"
@@ -134,13 +147,13 @@ mod tests {
 
     #[test]
     fn it_calcualtes_consumption() {
-        let (entries, bit_count) = parse_input(TEST_INPUT);
-        assert_eq!(calculate_consumption(entries, bit_count), 198);
+        let telemetry = Telemetry::from(TEST_INPUT);
+        assert_eq!(telemetry.consumption(), 198);
     }
 
     #[test]
     fn it_calculates_life_support() {
-        let (entries, bit_count) = parse_input(TEST_INPUT);
-        assert_eq!(calculate_life_support(entries, bit_count), 230);
+        let telemetry = Telemetry::from(TEST_INPUT);
+        assert_eq!(telemetry.life_support(), 230);
     }
 }
